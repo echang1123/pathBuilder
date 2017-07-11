c11a4e56e8ad91ea7b40ae6e954e672ed832a4f0
Eugene <exc8593@rit.edu>
2017-07-11 14:25:40 -0400

    first test commit!

---
+++ src/Players/EEVEE/EEVEE.java
@@ -0,0 +1,35 @@
+package Players.EEVEE;
+
+import Interface.PlayerModulePart1;
+import Interface.PlayerMove;
+
+/**
+ * Created by Eugene on 7/11/2017.
+ */
+public class EEVEE implements PlayerModulePart1{
+
+    @Override
+    public boolean hasWonGame(int i) {
+        return false;
+    }
+
+    @Override
+    public void initPlayer(int i, int i1) {
+
+    }
+
+    @Override
+    public void lastMove(PlayerMove playerMove) {
+
+    }
+
+    @Override
+    public void otherPlayerInvalidated() {
+
+    }
+
+    @Override
+    public PlayerMove move() {
+        return null;
+    }
+}
